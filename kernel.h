d8e2e7397430145795d6f77a2f5ef3265bcf9d6c
Recoletas <2309381783@qq.com>
2026-01-04 15:41:14 +0800

    panic

--- kernel.h
+++ kernel.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#define PANIC(fmt, ...)                                                        \
+    do {                                                                       \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {}                                                           \
+    } while (0)
+
 struct sbiret {
     long error;
     long value;
 };
+#define READ_CSR(reg)                                                          \
+    ({                                                                         \
+        unsigned long __tmp;                                                   \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
+        __tmp;                                                                 \
+    })
+#define WRITE_CSR(reg, value)                                                  \
+    do {                                                                       \
+        unsigned long __tmp = (unsigned long) (value);                         \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
+    } while (0)
