fc6e7e209a95eaab7901f004ceed2dd196449341
Recoletas <2309381783@qq.com>
2026-01-10 23:47:23 +0800

    yielld

--- kernel.c
+++ kernel.c
@@ -1,7 +1,123 @@
-#include "kernel.h"
 #include "common.h"
+#include "kernel.h"
+
+struct process {
+    int pid;             // 进程 ID
+    int state;           // 进程状态: PROC_UNUSED 或 PROC_RUNNABLE
+    vaddr_t sp;          // 栈指针
+    uint8_t stack[8192]; // 内核栈
+};
+#define PROCS_MAX 8       // 最大进程数量
+
+#define PROC_UNUSED   0   // 未使用的进程控制结构
+#define PROC_RUNNABLE 1   // 可运行的进程
+struct process *current_proc; // 当前运行的进程
+struct process *idle_proc;    // 空闲进程
+
+void yield(void) {
+    // 搜索可运行的进程
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++) {
+        struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+            next = proc;
+            break;
+        }
+    }
+
+    // 如果除了当前进程外没有可运行的进程，返回并继续处理
+    if (next == current_proc)
+        return;
+
+    // 上下文切换
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
+struct process procs[PROCS_MAX]; // 所有进程控制结构
+
+void putchar(char ch);
+void switch_context(uint32_t *prev_sp, uint32_t *next_sp);
+
+struct process *create_process(uint32_t pc) {
+    // 查找未使用的进程控制结构
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    // 设置被调用者保存的寄存器。这些寄存器值将在 switch_context 
+    // 中的第一次上下文切换时被恢复。
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    // 初始化字段
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
 
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+void delay(void) {
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop"); // 什么都不做
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) {
+    printf("starting process A\n");
+    while (1) {
+        putchar('A');
+        switch_context(&proc_a->sp, &proc_b->sp);
+        delay();
+    }
+}
+
+void proc_b_entry(void) {
+    printf("starting process B\n");
+    while (1) {
+        putchar('B');
+        switch_context(&proc_b->sp, &proc_a->sp);
+        delay();
+    }
+}
 
 void kernel_entry(void);
 // 1. SBI 调用实现
@@ -32,10 +148,11 @@ void putchar(char ch) {
 void kernel_main(void) { 
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    WRITE_CSR(stvec, (uint32_t) kernel_entry); 
-    __asm__ __volatile__("unimp"); 
-    printf("\n\nHello %s\n","World!");
-    printf("1+2=%d,%x\n",1+2,0x1234abcd);
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+    proc_a_entry();
    
 
     PANIC("booted!");
@@ -126,6 +243,47 @@ void kernel_entry(void) {
 }
 __attribute__((section(".text.boot")))
 __attribute__((naked))
+void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp) {
+    __asm__ __volatile__(
+        // 将被调用者保存寄存器保存到当前进程的栈上
+        "addi sp, sp, -13 * 4\n" // 为13个4字节寄存器分配栈空间 
+        "sw ra,  0  * 4(sp)\n"   // 仅保存被调用者保存的寄存器
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        // 切换栈指针
+        "sw sp, (a0)\n"         // *prev_sp = sp;
+        "lw sp, (a1)\n"         // 在这里切换栈指针(sp)
+
+        // 从下一个进程的栈中恢复被调用者保存的寄存器
+        "lw ra,  0  * 4(sp)\n"  // 仅恢复被调用者保存的寄存器
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"  // 我们已从栈中弹出13个4字节寄存器
+        "ret\n"
+    );
+}
 void boot(void) {
     __asm__ __volatile__(
         "mv sp, %[stack_top]\n"
